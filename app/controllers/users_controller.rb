a8c35081877e49015280f7d21cd45d9ffb34aab1
Schramm80 <carlos.schramm80@gmail.com>
2015-03-12 15:27:09 +0100

    Johannes and Carlos added users and social media profile controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,23 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all.to_json
-    render json: @users
+    
+    if request.path == "/users.json"
+      render json: @users
+    else 
+      render 'index'
+    end
     
     #Maybe add json pretty rendering?
   end
 
+  def show
+    id = (params[:id])
+   @user = User.find(id)
+   @social_media_profile = SocialMediaProfile.where(user_id: id)
+   # to do URL logic
+   @res = {user: @user, social_media_profile: @social_media_profile}
+   render json: @res
+ end
 
 end
