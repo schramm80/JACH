1d7cd83b055f2e31525569cdb977610961039402
Johannes <cyb@wigglytuff>
2015-03-12 15:02:06 +0100

    Fixed users plural error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = Users.all.to_json
+    @users = User.all.to_json
     render json: @users
     
     #Maybe add json pretty rendering?
