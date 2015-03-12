bc77faf1602b99ca5bd2c2ed0a8aa425ad53adf6
Johannes <cyb@wigglytuff>
2015-03-12 09:47:50 +0100

    Fixed indent

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,39 +1,38 @@
 class CampaignsController < ApplicationController
 
 
-def index
-  @campaigns = Campaign.all.to_json
-  render json: @campaigns
-  
-  #Maybe add json pretty rendering?
-end
+  def index
+    @campaigns = Campaign.all.to_json
+    render json: @campaigns
+    
+    #Maybe add json pretty rendering?
+  end
 
 
 
-def show
- @campaign = Campaign.find(params[:id])
- render json: @campaign
-end
-def home
-  
+  def show
+   @campaign = Campaign.find(params[:id])
+   render json: @campaign
+  end
 
-end
 
-def new
-  @campaign = Campaign.new
-end
+  def home
+  end
 
-def create
-  @campaign = Campaign.new(campaign_params)
+  def new
+    @campaign = Campaign.new
+  end
 
-  @campaign.save
-  redirect_to root_path
-end
+  def create
+    @campaign = Campaign.new(campaign_params)
 
-def edit
-  @campaign = Campaign.find(params[:id])
- 
-end
+    @campaign.save
+    redirect_to root_path
+  end
 
+  def edit
+    @campaign = Campaign.find(params[:id])
+   
+  end
 
 end
