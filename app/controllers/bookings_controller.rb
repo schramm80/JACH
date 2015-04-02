b4b300e6b66f1ff98f720393b21f3d2e4cc21441
Schramm80 <carlos.schramm80@gmail.com>
2015-04-02 09:31:03 +0200

    added the index and show file to booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,25 @@
 class BookingsController < ApplicationController
 
   def index
-    @bookings = Booking.all.to_json
-    
-    
+    @bookings = Booking.all.paginate(page: params[:page], per_page: 20)
+
     respond_to do |format|
-      format.html 
+      format.html
       format.json
     end
-    
   end
+
+  def show
+    @booking = Booking.find(params[:id])
+    puts @booking
+
+    respond_to do |format|
+      format.html
+      format.json
+    end
+
+ end
+
 
 
 end
